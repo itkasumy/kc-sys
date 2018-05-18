4367717cbb0a17818eaca8e0dc1404a04fb05ec2
kasumy <18500682038@163.com>
2018-05-18 11:31:48 +0800

    搭建首页

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="kcHeader"></div>
+    <div class="kcBody">
+      <div class="kcSideBar"></div>
+      <el-main>Main</el-main>
+    </div>
   </div>
 </template>
 
@@ -9,3 +13,20 @@ export default {
   name: 'App'
 }
 </script>
+
+<style lang="stylus" scoped>
+#app
+  height 100%
+  .kcHeader
+    height 40px
+    background #ff2200
+  .kcBody
+    display flex
+    height calc(100% - 40px)
+    .kcSideBar
+      width 200px
+      height 100%
+      background #006699
+    .el-main
+      padding 10px
+</style>
--- src/view/Entry/Entry.vue
+++ src/view/Entry/Entry.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quaerat dolorum doloremque expedita laborum cum dolor, eius repudiandae harum eum maxime tempora veritatis minima, vel numquam amet facilis libero doloribus voluptate a nesciunt. Saepe necessitatibus deserunt a, ut inventore eaque veritatis facilis reprehenderit consectetur doloremque nobis nulla aperiam architecto id explicabo earum, recusandae reiciendis molestias iusto similique alias quas omnis sunt. Adipisci odio quas sequi soluta fugiat veritatis repellendus perspiciatis deleniti illo mollitia in provident dolores, eos voluptatibus impedit vero fugit quaerat vitae quasi corrupti modi doloremque reprehenderit ipsum eveniet? Unde dicta sed optio numquam aliquid ratione in obcaecati reiciendis.
   </div>
 </template>
 
@@ -13,5 +12,4 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
 </style>
